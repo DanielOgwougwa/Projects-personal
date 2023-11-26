baf09203d499a61b9f7c8ddc532cfe33cf81bcb0
Daniel Ogwougwa <99253195+DanielOgwougwa@users.noreply.github.com>
2023-11-26 15:48:24 +0000

    Basic implementation of main menu

---
+++ CalorieProgram.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+// Main Menu //
+public class CalorieProgram{
+    
+    // Main Menu 
+    public static void main(String[] args) {
+        System.out.println("Welcome to the calorie program!");
+        System.out.println("This program will help you calculate your calories!");
+        System.out.println("Please choose one of the following options");
+        System.out.println("1: Weight gain");
+        System.out.println("2: Weight loss");
+        Scanner reader = new Scanner(System.in);    // Creates a Scanner object
+        String input = reader.nextLine();
+    }
+}
