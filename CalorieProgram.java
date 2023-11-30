9836b46943a2b94bfd314311703445e8d0d3c58b
Daniel Ogwougwa <99253195+DanielOgwougwa@users.noreply.github.com>
2023-11-30 15:18:54 +0000

    Minor changes to code readability of main menu

--- CalorieProgram.java
+++ CalorieProgram.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class CalorieProgram{
-    // Global Variables 
+    /*** Global Variables ***/
     int user_weight;
     int user_height;
     int user_age;
@@ -10,9 +10,16 @@ public class CalorieProgram{
     public static void main(String[] args) {
         System.out.println("Welcome to the calorie program!");
         System.out.println("This program will help you calculate your calories!");
+        System.out.println("To calculate your calories, you will need to enter some details!");
+
+        System.out.println("Please enter your age: ");      // Asks for users age 
+        Scanner ageReader = new Scanner(System.in);         
+        int user_age = ageReader.nextInt();           
+
         System.out.println("Please enter your weight: ");   // Asks for users weight
-        Scanner userinput = new Scanner(System.in);     // Takes users weight 
+        Scanner userinput = new Scanner(System.in);     
         int user_weight = userinput.nextInt();
+
         System.out.println("Please choose one of the following options");
         System.out.println("1: Weight gain");
         System.out.println("2: Weight loss");
