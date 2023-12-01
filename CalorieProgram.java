31275178c253a7ca33f852205c10b5a472b53a21
Daniel Ogwougwa <99253195+DanielOgwougwa@users.noreply.github.com>
2023-12-01 18:52:14 +0000

    Introductory details added to weightGain method

--- CalorieProgram.java
+++ CalorieProgram.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class CalorieProgram{
     /*** Global Variables ***/
-    int user_age;
-    int user_weight;
-    int user_height;
+    static int user_age;
+    static int user_weight;
+    static int user_height;
 
     // Main Menu 
     public static void main(String[] args) {
@@ -14,15 +14,15 @@ public class CalorieProgram{
 
         System.out.println("Please enter your age: ");      // Asks for users age 
         Scanner ageReader = new Scanner(System.in);         
-        int user_age = ageReader.nextInt();           
+        user_age = ageReader.nextInt();           
 
         System.out.println("Please enter your weight: ");   // Asks for users weight
         Scanner weightReader = new Scanner(System.in);     
-        int user_weight = weightReader.nextInt();
+        user_weight = weightReader.nextInt();
 
         System.out.println("Please enter your height: ");   // Asks for users height
         Scanner heightReader = new Scanner(System.in);     
-        int user_height = heightReader.nextInt();
+        user_height = heightReader.nextInt();
 
         System.out.println("Please choose one of the following options");
         System.out.println("1: Weight gain");
@@ -38,7 +38,9 @@ public class CalorieProgram{
     }
 
     public static void weightGain(){
-
+        System.out.println("In order, to lose weight we first need to calculate your calorie maintenance");
+        System.out.println("These are the details you have entered: ");
+        System.out.println("age: " + user_age + " weight: " + user_weight + " height: " + user_height);
     }
 
     public static void weightLoss(){
